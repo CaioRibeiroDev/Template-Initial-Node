f9672fc4c168618f5d4769cf69d76124beec2bf9
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2022-10-25 16:32:52 -0300

    implements apperror, database and tsyringe

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,25 @@
-import express from 'express'
+import cors from 'cors'
+import express, { NextFunction, Request, Response, urlencoded } from 'express'
+import 'express-async-errors'
+import 'reflect-metadata'
+import '../src/http/tsyringe'
+import { AppError } from './errors/AppError'
+import { routes } from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({ hello: 'world' })
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+app.use(cors())
+app.use(routes)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) return response.status(err.statusCode).json({ message: err.message })
+
+  return response.status(500).json({
+    status: 'Error',
+    message: `Internal server error - ${err.message}`
+  })
 })
 
 app.listen(3333)
